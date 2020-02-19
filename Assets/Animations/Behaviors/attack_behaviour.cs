921ce343b73ee96b3272357d521127fa2af8b05f
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-19 19:33:25 +0100

    File renamed

--- Assets/Animations/Behaviors/attack_behaviour.cs
+++ Assets/Animations/Behaviors/attack_behaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class attack_behavior : StateMachineBehaviour
+public class attack_behaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
