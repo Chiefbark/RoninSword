35b11b10316aac66f9a9ba7667491fc5dde921be
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-20 20:38:24 +0100

    Implemented boss behaviour

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class Boss : Enemy
 {
+    // Maximum number of clicks on the Boss (number of Minions in game)
+    public int MaxClick
+    {
+        get => maxClick;
+        set
+        {
+            maxClick = value;
+            for (int ii = 1; ii < maxClick * 2; ii += 2)
+                Indexes.Enqueue(ii);
+        }
+    }
+    private int maxClick;
+    // The indexes of the Boss, indicates the order
+    private Queue<int> Indexes = new Queue<int>();
+    private int nClick; // Click counter
+
     public override bool Kill(int expectedIndex)
     {
-        return true;
+        int index = Indexes.Dequeue();
+        // If both indexes are equal
+        if (expectedIndex == index)
+        {
+            // Delays the blood effect
+            // Once the blood effect is gone, it will active the death behaviour
+            //StartCoroutine(Blood());
+            return true;
+        }
+        // Enables the attack flag
+        Attack = true;
+        return false;
     }
 
     /// <summary>
@@ -21,5 +48,22 @@ public class Boss : Enemy
         blood.GetComponent<Animator>().SetFloat("speed", GameRuler.SPEED);
         blood.transform.SetParent(transform);
         blood.transform.localPosition = new Vector2(0, 0.2f);
+    }
+
+    protected override void OnMouseDown()
+    {
+        if (GameRuler.GAMESTATUS == GameRuler.GAME_STATUS_LIVE)
+        {
+            nClick++;
+            // Notifies the GameRuler that this Enemy has been clicked
+            GameObject.Find("GameRuler").GetComponent<GameRuler>().NotifyClick(this);
+            // Loads and places the select effect
+            GameObject select = (GameObject)Instantiate(Resources.Load("select"));
+            select.transform.SetParent(transform);
+            select.transform.localPosition = new Vector2(0, -0.3f);
+            if (nClick == MaxClick)
+                // Destroys the collider so the Enemy cannot be clicked again
+                Destroy(GetComponent<BoxCollider2D>());
+        }
     }
 }
