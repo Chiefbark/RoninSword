931cd4db85a8d91d71516f874c1c454ac11621ba
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-21 19:22:45 +0100

    Imlemented play, opti0ons & quit functions

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayMenu()
+    {
+        SceneManager.LoadScene(AppManager.GAME_SCENE);
+    }
+
+    public void OptionsMenu()
+    {
+
+    }
+
+    public void QuitMenu()
+    {
+        Application.Quit();
+    }
+}
