3474aceebeaa1d40e36d80a7b7b9635e4a71b5b4
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-21 19:57:41 +0100

    Added maximum & miniumum value to volume

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class AppManager
 {
-    public static float VOLUME_MUSIC;
-    public static float VOLUME_EFFECTS;
+    public static int VOLUME_MAX = 1;
+    public static int VOLUME_MIN = 0;
 
-    public static string GAME_SCENE = "GameScene";
-    public static string MENU_SCENE = "MainMenu";
+    public static float VOLUME_MUSIC = 1;
+    public static float VOLUME_EFFECTS = 1;
+
+    public static string SCENE_GAME = "GameScene";
+    public static string SCENE_MENU = "MainMenu";
 }
