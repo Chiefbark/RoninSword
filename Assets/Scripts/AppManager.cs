9051b876201fd7744521d1c1d00ea9419f67fa6d
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-21 19:22:29 +0100

    Added AppManager script

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager
+{
+    public static float GAME_VOLUME_MUSIC;
+    public static float GAME_VOLUME_EFFECTS;
+
+    public static string GAME_SCENE = "GameScene";
+    public static string MENU_SCENE = "MainMenu";
+}
