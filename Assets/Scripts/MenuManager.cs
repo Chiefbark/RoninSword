172bd85f4637c26f07608ed597d444e8de84d8d5
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-21 20:19:04 +0100

    Added comments

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -21,18 +21,44 @@ public class MenuManager : MonoBehaviour
         GameObject.Find("Background").GetComponent<Image>().material.mainTextureOffset += new Vector2(0.0005f, 0);
     }
 
-    public void PlayMenu()
+    /// <summary>
+    /// Handles the behaviour of the button Play
+    /// </summary>
+    public void OnPlayClicked()
     {
         SceneManager.LoadScene(AppManager.SCENE_GAME);
     }
 
-    public void OptionsMenu()
+    /// <summary>
+    /// Handles the behaviour of the button Options
+    /// </summary>
+    public void OnOptionsClicked()
     {
 
     }
 
-    public void QuitMenu()
+    /// <summary>
+    /// Handles the behaviour of the button Quit
+    /// </summary>
+    public void OnQuitClicked()
     {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// Handles the biehaviour of the button Sound
+    /// </summary>
+    public void OnVolumeClicked()
+    {
+        if (AppManager.VOLUME == AppManager.VOLUME_MAX)
+        {
+            AppManager.VOLUME = AppManager.VOLUME_MIN;
+            GameObject.Find("Sound").GetComponent<Image>().sprite = Resources.Load<Sprite>("Icons/volume_off");
+        }
+        else
+        {
+            AppManager.VOLUME = AppManager.VOLUME_MAX;
+            GameObject.Find("Sound").GetComponent<Image>().sprite = Resources.Load<Sprite>("Icons/volume_up");
+        }
     }
 }
