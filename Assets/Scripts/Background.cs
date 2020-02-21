0db69ee834fdf203b40e8411a7513eddf880c90f
Chiefbark <jorge.chercoles.97@gmail.com>
2020-02-21 14:11:26 +0100

    Added background behaviour script

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Material m in GetComponent<Renderer>().materials)
+            m.mainTextureOffset += new Vector2(0, 0.005f * GameRuler.SPEED);
+    }
+}
